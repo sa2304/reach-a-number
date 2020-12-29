0fafb94505bf9c7182b55ce3761a393b149248ef
Антон Шмидт <schmidt.ajax@gmail.com>
2020-12-29 12:29:16 +0700

    Constant-time math solution

--- main.cpp
+++ main.cpp
@@ -8,5 +8,7 @@ int main()
 {
   Solution s;
   cout << s.reachNumber(2) << endl;
+  cout << s.reachNumber(-1'000) << endl;
+  cout << s.reachNumber(-1'000'000) << endl;
   return 0;
 }
--- solution.h
+++ solution.h
@@ -1,45 +1,16 @@
 #ifndef SOLUTION_H
 #define SOLUTION_H
 
-#include <queue>
-#include <set>
-#include <vector>
-
 class Solution {
 public:
   int reachNumber(int target) {
-    std::queue<int> bfs_queue;
-    std::set<int> visited;
-
-    bfs_queue.push(0);
-    visited.insert(0);
-    int level = 0;
-    while (!bfs_queue.empty()) {
-      int count = bfs_queue.size();
-      while (count--) {
-        int index = bfs_queue.front();
-        bfs_queue.pop();
-        if (index == target) {
-          return level;
-        }
-
-        const int n = level + 1;
-        int left = index - n;
-        if (!visited.count(left)) {
-          bfs_queue.push(left);
-          visited.insert(left);
-        }
-
-        int right = index + n;
-        if (!visited.count(right)) {
-          bfs_queue.push(right);
-          visited.insert(right);
-        }
-      }
-      ++level;
+    target = std::abs(target);
+    int k = 0;
+    while (0 < target) {
+      target -= ++k;
     }
 
-    return level;
+    return target % 2 == 0 ? k : k + 1 + k % 2;
   }
 
 };
